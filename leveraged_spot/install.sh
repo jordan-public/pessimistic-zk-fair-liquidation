8775e0db617d00263e75c9c62efe860321851cf9
jordan <jordan@bitola.com>
2025-06-21 23:56:57 +0200

    token_registry workaround (as I filed issue 5 months ago) + program install script

---
+++ leveraged_spot/install.sh
@@ -0,0 +1,27 @@
+#!/bin/zsh
+set -e
+set -x
+
+# Run ./run_node.sh to start the node before running this script
+
+# cd token_registry_workaround
+# ./install.sh
+
+#!/bin/zsh
+set -e
+set -x
+
+# Run ./run_node.sh to start the node before running this script
+
+pushd ../token_registry_workaround
+./install.sh
+sleep 10
+popd
+
+. ./.env
+
+leo clean
+leo build
+
+# leo deploy --yes
+leo deploy --yes --path . --network $NETWORK --endpoint $ENDPOINT --private-key $PRIVATE_KEY
